c4f1fdf1e5f09cecc91bb79a5846c4b6c8a65947
Danish <danids@mail.ru>
2020-10-04 13:47:19 +0300

    some change in Homework_7_1.py

--- Homework_7_1.py
+++ Homework_7_1.py
@@ -2,21 +2,16 @@
 1. Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора
 класса (метод __init__()), который должен принимать данные (список списков)
 для формирования матрицы.
+Следующий шаг — реализовать перегрузку метода __str__() для вывода матрицы
+в привычном виде.
+Далее реализовать перегрузку метода __add__() для реализации операции сложения
+двух объектов класса Matrix (двух матриц). Результатом сложения должна быть
+новая матрица.
 """
 
-
-# def creator(matr):
-# def wrapper(itm):
-# matr(itm)
-# for i in itm:
-#  for j in i:
-# print(f'|  {j} ', end=' ')
-#  print('\n')
-
-# return wrapper
+from itertools import zip_longest
 
 
-# @creator
 class Matrix:
     def __init__(self, data):
         self.data = data
@@ -24,12 +19,22 @@ class Matrix:
     def __str__(self):
         result = ''
         for itm in self.data:
-            result += ' '.join(map(str, itm)) + '\n'
+            result += f'| {" | ".join(map(str, itm))} | \n'
         return result
 
+    def __add__(self, other):
+        res = []
+        for row_dat, row_oth in list(zip_longest(self.data, other.data, fillvalue=0)):
+            row = []
+            for el_dat, el_oth in list(zip_longest(row_dat, row_oth, fillvalue=0)):
+                row.append(el_dat + el_oth)
+            res.append(row)
+        return Matrix(res)
 
-tab1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-tab2 = Matrix([[3, 2, 1], [6, 5, 4], [9, 8, 7]])
+
+tab1 = Matrix([[1, 2, 3], [5, 6], [7, 8, 9]])
+tab2 = Matrix([[3, 1], [6, 5, 4], [9, 8]])
 print(tab1)
 print(tab2)
+
 print(tab1 + tab2)
